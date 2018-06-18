12399aea8e3d83cd43b12a26e43fae81852a7280
George Beckert <georgebeckert@gmail.com>
2018-06-18 15:46:21 -0400

    more h prep

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -1,17 +1,17 @@
 "use strict"
 const MongoClient = require("mongodb").MongoClient;
-const localPort = "mongodb://localhost:27017/";
-
+const localPort = "mongodb://localhost:27017/" ;
+const dbName = "chatdb" | process.env.MONGODB_URI.slice.substring(process.env.MONGODB_URI.lastIndexOf("/"),pocess.env.MONGODB_URI.length-1);
+console.log(dbName);
 
 const settings = {
     mongoConfig: {
         serverUrl: localPort,
-        database: "chatdb"
+        database: dbName
     }
 };
 
-let fullMongoUrl = process.env.MONGODB_URI ;
-//|| settings.mongoConfig.serverUrl + settings.mongoConfig.database;
+let fullMongoUrl = process.env.MONGODB_URI || settings.mongoConfig.serverUrl + settings.mongoConfig.database;
 console.log(`the mongo URL is ${fullMongoUrl}`)
 let _connection = undefined
 let connectDb = async () => {
