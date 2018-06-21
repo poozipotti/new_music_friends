a76a2b07edf78b3d13976ab7feb69efd7b138ebe
George Beckert <georgebeckert@gmail.com>
2018-06-21 15:33:45 -0400

    heroku prep

--- client/src/App.js
+++ client/src/App.js
@@ -37,6 +37,8 @@ class App extends Component {
 		let queries = queryString.parse((window.location.href.split("?").length == 2) ? window.location.href.split("?")[1] : "");
 		if(!queries.code){ //we dont want to update the code from spotify into the redirect
 			spotifyClientParams.redirect_uri = window.location.href;
+			spotifyClientParams.redirect_uri = spotifyClientParams.redirect_uri.substring(0,spotifyClientParams.indexOf("?")-1);
+			console.log("\n\n\nspotifyCleintParams.redirect_uri:" + spotifyClientParams.redirect_uri)
 		}
 		if(sessionId){
 			try{
